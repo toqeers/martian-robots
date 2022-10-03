f7bce39a2102bd1f7ade75ad77e037a6e1d79a3e
Toqeer Sardar <toqeer.sardar@gmail.com>
2022-10-03 12:00:03 +0100

    added some validation

--- src/main/java/org/ts/robot/model/Grid.java
+++ src/main/java/org/ts/robot/model/Grid.java
@@ -7,15 +7,18 @@ import java.util.Map;
 
 public final class Grid {
     private final GridPoint maxPoint;
+    private final GridPoint minPoint = new GridPoint(0, 0);
     private final Map<GridPoint, Boolean> scents = new HashMap<>();
 
     public Grid(@NonNull GridPoint maxPoint) {
+        if (maxPoint.equals(minPoint) || maxPoint.isLessThan(minPoint)) {
+            throw new IllegalArgumentException("Invalid max point: " + maxPoint + "should be more than: " + minPoint);
+        }
         this.maxPoint = maxPoint;
     }
 
     public boolean isOffTheGrid(@NonNull GridPoint point) {
-        return point.getX() < 0 || point.getY() < 0 ||
-                point.getX() > maxPoint.getX() || point.getY() > maxPoint.getY();
+        return point.isLessThan(minPoint) || point.isGreaterThan(maxPoint);
     }
 
     public boolean isScented(@NonNull GridPoint point) {
--- src/main/java/org/ts/robot/model/GridPoint.java
+++ src/main/java/org/ts/robot/model/GridPoint.java
@@ -6,4 +6,12 @@ import lombok.Value;
 public class GridPoint {
     int x;
     int y;
+
+    public boolean isLessThan(GridPoint point) {
+        return this.x < point.x || this.y < point.y;
+    }
+
+    public boolean isGreaterThan(GridPoint point) {
+        return this.x > point.x || this.y > point.y;
+    }
 }
